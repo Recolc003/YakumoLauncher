86998c319603891f1b5406b66e7b7bef71a87f9d
Recolc003 <kcxxp188@yahooc.coj.p>
2020-04-27 23:09:42 +0900

    ・メモ機能を専用のクラスに集約 ・Formデザイナーでエラーが出ていたので、Designerクラスから直接メモ用のクラスを参照しないように変更

---
+++ YakumoLauncher/Constant/ProvisionalConstantClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YakumoLauncher.Constant
+{
+    //暫定でここに定数定義しておく
+    static public class ProvisionalConstantClass
+    {
+        static public string MemoFileName = ".\\memo.txt";
+    }
+}
